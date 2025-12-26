9ba2adb183b10a4033cce66ad3868ec9907aeaba
Hunter <hunter@example.com>
2025-12-26 12:40:19 +0200

    feat: refresh icon and set app short name to NHA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
   appleWebApp: {
     capable: true,
     statusBarStyle: "black-translucent",
-    title: "Hunters",
+    title: "NHA",
   },
   formatDetection: {
     telephone: false,
